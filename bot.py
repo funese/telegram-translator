63c52fcd39f8666034d5ebf41b394ae17a0ff718
emiliano <funes.emiliano@outlook.com>
2021-07-28 17:30:33 -0300

    code cleaning

--- bot.py
+++ bot.py
@@ -86,10 +86,7 @@ def error(update, context):
 def main():
     """Start the bot."""
 
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    print("jelo")
+    # Create the Updater
     updater = Updater(bot_token, use_context=True)
 
     # Get the dispatcher to register handlers
@@ -98,27 +95,16 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    #dp.add_handler(MessageHandler(Filters.text, echo))
-
     dp.add_handler(MessageHandler(Filters.text, translate))
 
     # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
-    #updater.start_polling()
-
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=bot_token,
                           webhook_url="https://emitrbot.herokuapp.com/" + bot_token)
-    #updater.bot.set_webhook("https://emitrbot.herokuapp.com/" + bot_token)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+                          
     updater.idle()
 
 
