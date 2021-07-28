bef3bd7f3d5f7b4a614d9125ecf73d4ec419680a
emiliano <funes.emiliano@outlook.com>
2021-07-28 16:52:00 -0300

    changed webhook_url

--- bot.py
+++ bot.py
@@ -113,8 +113,8 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=bot_token)
-                          #webhook_url="https://emitrbot.herokuapp.com/" + TOKEN)
-    updater.bot.set_webhook("https://emitrbot.herokuapp.com/" + bot_token)
+                          webhook_url="https://emitrbot.herokuapp.com/" + TOKEN)
+    #updater.bot.set_webhook("https://emitrbot.herokuapp.com/" + bot_token)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
